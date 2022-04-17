8b8ea170aab7d2f4e24a1719ef6a609973c821c6
Terry Wan <JarodWan@users.noreply.github.com>
2022-04-17 11:33:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+src-git small https://github.com/kenzok8/small-package.git
+src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git
